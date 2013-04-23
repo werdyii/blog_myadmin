725ed099526ad9c241d1068012e6b414416b8529
Gusepe Werdy <gusepe.werdy@gmail.com>
2013-04-23 22:34:41 +0200

    Create asociation a Category_to_Post and seeds data

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130423165714) do
+ActiveRecord::Schema.define(:version => 20130423201945) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -22,8 +22,9 @@ ActiveRecord::Schema.define(:version => 20130423165714) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.integer  "category_id"
   end
 
 end
