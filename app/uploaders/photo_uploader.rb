49bc011c790a38c1c8c38cf07658dd99f6c3a6ee
Gusepe Werdy <gusepe.werdy@gmail.com>
2013-04-29 09:04:11 +0200

    Modify photo_uploader limit upload picture 800x600

--- app/uploaders/photo_uploader.rb
+++ app/uploaders/photo_uploader.rb
@@ -30,6 +30,8 @@ class PhotoUploader < CarrierWave::Uploader::Base
 
   # Process files as they are uploaded:
   # process :scale => [200, 300]
+  process :resize_to_limit => [800, 600]
+  
   #
   # def scale(width, height)
   #   # do something
